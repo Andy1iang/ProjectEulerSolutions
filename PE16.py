4743bec555af4ff6d7500dbcbe8031abc78c8158
Andy1iang <andyliang254@gmail.com>
2023-12-08 20:17:06 -0500

    Added Solution to Question 16

---
+++ PE16.py
@@ -0,0 +1,13 @@
+#https://projecteuler.net/problem=16
+
+from TimeCode import timeCode
+
+def projectEuler16():
+    
+    num = 2**1000
+    num = str(num)
+    num = list(map(int,list(num)))
+
+    print(sum(num))
+    
+timeCode(projectEuler16)
